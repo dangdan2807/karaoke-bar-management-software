5bf3e76e47adee7b795d87828c821d6f5d258e9c
KayJuno <dangdan2807@gmail.com>
2021-10-04 15:00:52 +0700

    update database

--- database/database.sql
+++ database/database.sql
@@ -1,18 +1,18 @@
 USE master
 GO
 
-CREATE DATABASE QuanLyKhachSan
+CREATE DATABASE QuanLyQuanKaraoke
 GO
 
--- Drop DATABASE QuanLyKhachSan
+-- Drop DATABASE QuanLyQuanKaraoke
 -- GO
 
-USE QuanLyKhachSan 
+USE QuanLyQuanKaraoke 
 GO
 
 CREATE TABLE TaiKhoan
 (
-    taiKhoan VARCHAR(50) NOT NULL PRIMARY KEY,
+    tenDangNhap VARCHAR(100) NOT NULL PRIMARY KEY,
     matKhau VARCHAR(100) NOT NULL DEFAULT(N'123456'),
     -- 1. kích hoạt || 0. vô hiệu hóa
     tinhTrang BIT NOT NULL DEFAULT(1)
@@ -26,13 +26,15 @@ CREATE TABLE NhanVien
     hoTen NVARCHAR(100) NOT NULL DEFAULT(N''),
     ngaySinh DATETIME,
     -- 1. nam | 0. nữ
-    gioiTinh BIT NOT NULL,
     soDienThoai VARCHAR(10),
     chucVu NVARCHAR(100) NOT NULL,
     mucLuong FLOAT NOT NULL,
-    taiKhoan VARCHAR(50) NOT NULL,
+    gioiTinh BIT NOT NULL,
+    trangThai NVARCHAR(100) NULL,
+    
+    taiKhoan VARCHAR(100) NOT NULL,
 
-    FOREIGN KEY (taiKhoan) REFERENCES dbo.TaiKhoan(taiKhoan)
+    FOREIGN KEY (taiKhoan) REFERENCES dbo.TaiKhoan(tenDangNhap)
 )
 GO
 
@@ -42,24 +44,9 @@ CREATE TABLE KhachHang
     cmnd VARCHAR(12) NOT NULL,
     hoTen NVARCHAR(100) NOT NULL DEFAULT(N''),
     -- 1. nam | 0. nữ
-    gioiTinh BIT NOT NULL,
     soDienThoai VARCHAR(10),
-    ngaySinh DATETIME
-)
-GO
-
-CREATE TABLE HoaDon
-(
-    maHoaDon INT IDENTITY PRIMARY KEY,
-    ngayGioDat DATETIME NOT NULL DEFAULT(getdate()),
-    -- 0. chưa thanh toán | 1. đã thanh toán | 2. đặt phòng
-    tinhTrang INT NOT NULL DEFAULT(0),
-
-    maNhanVien VARCHAR(10) NOT NULL,
-    maKH VARCHAR(10) NOT NULL,
-
-    FOREIGN KEY (maNhanVien) REFERENCES dbo.NhanVien (maNhanVien),
-    FOREIGN KEY (maKH) REFERENCES dbo.KhachHang (maKH)
+    ngaySinh DATETIME,
+    gioiTinh BIT NOT NULL
 )
 GO
 
@@ -69,19 +56,6 @@ CREATE TABLE DichVu
     tenDichVu NVARCHAR(100) NOT NULL DEFAULT(N''),
     giaBan FLOAT NOT NULL DEFAULT(0),
     soLuongTon INT NOT NULL DEFAULT(0)
-)
-GO
-
-CREATE TABLE CTDichVu
-(
-    maCTDichVu INT IDENTITY PRIMARY KEY,
-    soLuongDat INT NOT NULL DEFAULT(1),
-    ngayGioDat DATETIME NOT NULL DEFAULT(getdate()),
-    maDichVu VARCHAR(5) NOT NULL,
-    maHoaDon INT NOT NULL,
-
-    FOREIGN KEY (maDichVU) REFERENCES dbo.DichVu (maDichVu),
-    FOREIGN KEY (maHoaDon) REFERENCES dbo.HoaDon (maHoaDon)
 )
 GO
 
@@ -109,14 +83,46 @@ GO
 
 CREATE TABLE CTPhong
 (
-    maCTPhong INT IDENTITY PRIMARY KEY,
+    maCTPhong INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
     ngayGioNhan DATETIME NOT NULL,
     ngayGioTra DATETIME NOT NULL,
     maPhong VARCHAR(5) NOT NULL,
-    maHoaDon INT NOT NULL,
-
+    tienPhong FLOAT NOT NULL DEFAULT(0),
     FOREIGN KEY (maPhong) REFERENCES dbo.Phong (maPhong),
-    FOREIGN KEY (maHoaDon) REFERENCES dbo.HoaDon (maHoaDon)
+)
+GO
+
+CREATE TABLE HoaDon
+(
+    maHoaDon INT IDENTITY(1, 1) PRIMARY KEY,
+    ngayGioDat DATETIME NOT NULL DEFAULT(getdate()),
+    -- 0. chưa thanh toán | 1. đã thanh toán | 2. đặt phòng
+    tinhTrang INT NOT NULL DEFAULT(0),
+    TongTien FLOAT NOT NULL DEFAULT(0),
+    maNhanVien VARCHAR(10) NOT NULL,
+    maKH VARCHAR(10) NOT NULL,
+    maCTPhong INT NOT NULL,
+
+    FOREIGN KEY (maNhanVien) REFERENCES dbo.NhanVien (maNhanVien),
+    FOREIGN KEY (maKH) REFERENCES dbo.KhachHang (maKH),
+    FOREIGN KEY (maCTPhong) REFERENCES dbo.CTPhong (maCTPhong)
+)
+GO
+
+CREATE TABLE CTDichVu
+(
+    maDichVu VARCHAR(5) NOT NULL,
+    maHoaDon INT NOT NULL,
+    soLuongDat INT NOT NULL DEFAULT(1),
+    ngayGioDat DATETIME NOT NULL DEFAULT(getdate()),
+    tienDichVu FLOAT NOT NULL DEFAULT(0),
+    PRIMARY KEY (maDichVu, maHoaDon),
+    FOREIGN KEY
+    (maDichVU) REFERENCES dbo.DichVu
+    (maDichVu),
+    FOREIGN KEY
+    (maHoaDon) REFERENCES dbo.HoaDon
+    (maHoaDon)
 )
 GO
 
@@ -204,7 +210,7 @@ VALUES
 GO
 
 INSERT INTO dbo.TaiKhoan
-    (taiKhoan, matKhau, tinhTrang)
+    (tenDangNhap, matKhau, tinhTrang)
 VALUES
     ('phamdangdan', '123456', 1),
     ('huynhtuananh', '123456', 1),
@@ -213,53 +219,55 @@ VALUES
     ('nhanvien1', '123456', 1),
     ('nhanvien2', '123456', 1),
     ('nhanvien3', '123456', 1),
-    ('nhanvien4', '123456', 1)
+    ('nhanvien4', '123456', 1),
+    ('nhanvien5', '123456', 0)
 GO
 
 INSERT INTO dbo.NhanVien
-    (maNhanVien, cmnd, hoTen, ngaySinh, gioiTinh, soDienThoai, chucVu, mucLuong, taiKhoan)
+    (maNhanVien, cmnd, hoTen, ngaySinh, gioiTinh, soDienThoai, chucVu, mucLuong, taiKhoan, trangThai)
 VALUES
-    ('NV00000001', '111111111', N'Phạm Đăng Đan', '2001-01-01', 1, '0312345678', N'Quản lý', 6000000, 'phamdangdan'),
-    ('NV00000002', '111111113', N'Huỳnh Tuấn Anh', '2001-01-01', 1, '0312345671', N'Quản lý', 6000000, 'huynhtuananh'),
-    ('NV00000003', '111111112', N'Lăng Nhật Sang', '2001-01-01', 1, '0312345679', N'Quản lý', 6000000, 'langnhapsang'),
-    ('NV00000004', '111111114', N'Võ Minh Hiếu', '2001-01-01', 1, '0312345672', N'Quản lý', 6000000, 'vominhhieu'),
-    ('NV00000005', '111111115', N'Nguyễn Xuân Anh', '1999-06-14', 0, '0312345673', N'Nhân Viên', 3000000, 'nhanvien1'),
-    ('NV00000006', '111111116', N'Trần Thị Ngọc Vân', '1998-07-17', 0, '0812144673', N'Nhân Viên', 2800000, 'nhanvien2'),
-    ('NV00000007', '111111117', N'Trần Vinh Can', '1993-08-27', 1, '0715344673', N'Nhân Viên', 3100000, 'nhanvien3'),
-    ('NV00000008', '111111118', N'Bùi Ngọc Văn', '1996-12-23', 1, '0532344647', N'Nhân Viên', 2700000, 'nhanvien4')
+    ('NV00000001', '111111111', N'Phạm Đăng Đan', '2001-01-01', 1, '0312345678', N'Quản lý', 6000000, 'phamdangdan', N''),
+    ('NV00000002', '111111113', N'Huỳnh Tuấn Anh', '2001-01-01', 1, '0312345671', N'Quản lý', 6000000, 'huynhtuananh', N''),
+    ('NV00000003', '111111112', N'Lăng Nhật Sang', '2001-01-01', 1, '0312345679', N'Quản lý', 6000000, 'langnhapsang', N''),
+    ('NV00000004', '111111114', N'Võ Minh Hiếu', '2001-01-01', 1, '0312345672', N'Quản lý', 6000000, 'vominhhieu', N''),
+    ('NV00000005', '111111115', N'Nguyễn Xuân Anh', '1999-06-14', 0, '0312345673', N'Nhân Viên', 3000000, 'nhanvien1', N''),
+    ('NV00000006', '111111116', N'Trần Thị Ngọc Vân', '1998-07-17', 0, '0812144673', N'Nhân Viên', 2800000, 'nhanvien2', N''),
+    ('NV00000007', '111111117', N'Trần Vinh Can', '1993-08-27', 1, '0715344673', N'Nhân Viên', 3100000, 'nhanvien3', N''),
+    ('NV00000008', '111111118', N'Bùi Ngọc Văn', '1996-12-23', 1, '0532344647', N'Nhân Viên', 2700000, 'nhanvien4', N''),
+    ('NV00000009', '111111119', N'Bùi Văn Xuân', '1996-06-23', 1, '0532234647', N'Nhân Viên', 2700000, 'nhanvien5', N'Nghỉ việc')
 GO
 
 INSERT INTO dbo.KhachHang
     (maKH, cmnd, hoTen, gioiTinh, soDienThoai, ngaySinh)
 VALUES
-    ('KH00000001', '211111111', N'Nguyễn Việt Nam', 1, '0653684411', '1987-09-15'),
-    ('KH00000002', '211111112', N'Nguyễn Huỳnh Dũng', 1, '0468265445', '1989-07-24'),
-    ('KH00000003', '211111113', N'Ngô Quốc Cường', 1, '0663473239', '1999-07-08'),
-    ('KH00000004', '211111114', N'Nguyễn Thị Hảo', 0, '0662426330', '1978-06-20'),
-    ('KH00000005', '211111115', N'Nguyễn Thị Hậu', 0, '0606411930', '1968-01-09'),
-    ('KH00000006', '211111116', N'Nguyễn Vũ Lâm Đức', 1, '0624819104', '1993-09-06'),
-    ('KH00000007', '211111117', N'Phan Thị Thủy', 0, '0632127202', '1987-06-10'),
-    ('KH00000008', '211111118', N'Phạm Văn Duy', 1, '0682290550', '1992-12-05'),
-    ('KH00000009', '211111119', N'Hoàng Tiến Hào', 1, '0646311277', '1998-03-02'),
-    ('KH00000010', '211111120', N'Dương Ngọc Vy', 1, '0646221277', '1993-14-06'),
-    ('KH00000011', '211111121', N'Nguyễn Chí Hoàng', 1, '0679042843', '1984-08-05'),
-    ('KH00000012', '211111122', N'Đỗ Huy Hữu', 1, '0669648870', '1985-09-18'),
-    ('KH00000013', '211111123', N'Nguyễn Anh Khoa', 0, '0677723969', '1995-08-02'),
-    ('KH00000014', '211111124', N'Trần Anh Lâm', 0, '0686439811', '1973-08-22'),
-    ('KH00000015', '211111125', N'Nguyễn Thái Huấn', 1, '0644708156', '1990-08-10'),
-    ('KH00000016', '211111126', N'Nguyễn Dương Lập', 1, '0617190242', '1988-10-28'),
-    ('KH00000017', '211111127', N'Nguyễn Văn Nghị', 1, '0664505419', '1994-12-24'),
-    ('KH00000018', '211111128', N'Trần Hoài Phương', 1, '0602642649', '1982-05-28'),
-    ('KH00000019', '211111129', N'Đinh Diệp Vy', 0, '0684218383', '1983-11-16'),
-    ('KH00000020', '211111130', N'Hoàng Thị Hồng', 0, '0678763637', '1985-07-29')
+    ('KH00000001', '200000001', N'Nguyễn Việt Nam', 1, '0300000001', '1987-09-15'),
+    ('KH00000002', '200000002', N'Nguyễn Huỳnh Dũng', 1, '0300000002', '1989-07-24'),
+    ('KH00000003', '200000003', N'Ngô Quốc Cường', 1, '0300000003', '1999-07-08'),
+    ('KH00000004', '200000004', N'Nguyễn Thị Hảo', 0, '0300000004', '1978-06-20'),
+    ('KH00000005', '200000005', N'Nguyễn Thị Hậu', 0, '0300000005', '1968-01-09'),
+    ('KH00000006', '200000006', N'Nguyễn Vũ Lâm Đức', 1, '0300000006', '1993-09-06'),
+    ('KH00000007', '200000007', N'Phan Thị Thủy', 0, '0300000007', '1987-06-10'),
+    ('KH00000008', '200000008', N'Phạm Văn Duy', 1, '0300000008', '1992-12-05'),
+    ('KH00000009', '200000009', N'Hoàng Tiến Hào', 1, '0300000009', '1998-03-02'),
+    ('KH00000010', '200000010', N'Dương Ngọc Vy', 1, '0300000010', '1993-06-14'),
+    ('KH00000011', '200000011', N'Nguyễn Chí Hoàng', 1, '0300000011', '1984-08-05'),
+    ('KH00000012', '200000012', N'Đỗ Huy Hữu', 1, '0300000012', '1985-09-18'),
+    ('KH00000013', '200000013', N'Nguyễn Anh Khoa', 0, '0300000013', '1995-08-02'),
+    ('KH00000014', '200000014', N'Trần Anh Lâm', 0, '0300000014', '1973-08-22'),
+    ('KH00000015', '200000015', N'Nguyễn Thái Huấn', 1, '0300000015', '1990-08-10'),
+    ('KH00000016', '200000016', N'Nguyễn Dương Lập', 1, '0300000016', '1988-10-28'),
+    ('KH00000017', '200000017', N'Nguyễn Văn Nghị', 1, '0300000017', '1994-12-24'),
+    ('KH00000018', '200000018', N'Trần Hoài Phương', 1, '0300000018', '1982-05-28'),
+    ('KH00000019', '200000019', N'Đinh Diệp Vy', 0, '0300000019', '1983-11-16'),
+    ('KH00000020', '200000020', N'Hoàng Thị Hồng', 0, '0300000020', '1985-07-29')
 GO
 
 INSERT INTO dbo.LoaiPhong
     (maLP, tenLP, sucChua, giaTien)
 VALUES
-    ('LP001', N'Phòng 5', 5, 80000),
-    ('LP002', N'Phòng 10', 10, 100000),
-    ('LP003', N'Phòng 15', 15, 150000)
+    ('LP001', N'Phòng 5 người', 5, 80000),
+    ('LP002', N'Phòng 10 người', 10, 100000),
+    ('LP003', N'Phòng 15 người', 15, 150000)
 GO
 
 INSERT INTO dbo.Phong
@@ -282,42 +290,43 @@ VALUES
     ('P0015', 1, N'Tầng 3', 'LP003')
 GO
 
-INSERT INTO dbo.HoaDon
-    (ngayGioDat, tinhTrang, maNhanVien, maKH)
-VALUES
-    ('2021-10-01 10:00:00', 1, 'NV00000001', 'KH00000001'),
-    ('2021-10-02 12:00:00', 1, 'NV00000002', 'KH00000002'),
-    ('2021-10-01 15:00:00', 1, 'NV00000003', 'KH00000003'),
-    ('2021-10-01 15:30:00', 1, 'NV00000004', 'KH00000004')
-GO
 
 INSERT INTO dbo.CTPhong
-    (ngayGioNhan, ngayGioTra, maPhong, maHoaDon)
+    (ngayGioNhan, ngayGioTra, maPhong, tienPhong)
 VALUES
-    ('2021-10-01 10:00:00', '2021-10-01 12:00:00', 'P0001', 1),
-    ('2021-10-02 12:00:00', '2021-10-02 13:00:00', 'P0002', 2),
-    ('2021-10-01 15:00:00', '2021-10-01 17:00:00', 'P0003', 3),
-    ('2021-10-01 15:30:00', '2021-10-01 18:10:00', 'P0004', 4)
+    ('2021-10-01 10:00:00', '2021-10-01 12:00:00', 'P0001', 160000),
+    ('2021-10-01 15:00:00', '2021-10-01 17:00:00', 'P0003', 160000),
+    ('2021-10-01 15:30:00', '2021-10-01 18:10:00', 'P0004', 213280),
+    ('2021-10-02 12:00:00', '2021-10-02 13:00:00', 'P0002', 80000)
+GO
+
+INSERT INTO dbo.HoaDon
+    (ngayGioDat, tinhTrang, TongTien, maNhanVien, maKH, maCTPhong)
+VALUES
+    ('2021-10-01 10:00:00', 1, 0.0, 'NV00000001', 'KH00000001', 1),
+    ('2021-10-01 15:00:00', 1, 0.0, 'NV00000003', 'KH00000003', 2),
+    ('2021-10-01 15:30:00', 1, 0.0, 'NV00000004', 'KH00000004', 3),
+    ('2021-10-02 12:00:00', 1, 0.0, 'NV00000002', 'KH00000002', 4)
 GO
 
 INSERT INTO dbo.CTDichVu
-    (soLuongDat, ngayGioDat, maDichVu, maHoaDon)
+    (maDichVu, maHoaDon, soLuongDat, ngayGioDat, tienDichVu)
 VALUES
-    (1, '2021-10-01 10:05:00', 'DV001', 1),
-    (2, '2021-10-01 10:05:00', 'DV002', 1),
-    (2, '2021-10-01 11:00:00', 'DV003', 1),
-    
-    (1, '2021-10-02 12:10:00', 'DV005', 2),
-    (2, '2021-10-02 12:10:00', 'DV002', 2),
-    (4, '2021-10-02 12:10:00', 'DV001', 2),
-    
-    (2, '2021-10-01 15:01:00', 'DV003', 3),
-    (6, '2021-10-01 15:30:00', 'DV002', 3),
-    (1, '2021-10-01 16:02:00', 'DV003', 3),
+    ('DV001', 1, 1, '2021-10-01 10:05:00', 30000),
+    ('DV002', 1, 2, '2021-10-01 10:05:00', 70000),
+    ('DV003', 1, 2, '2021-10-01 11:00:00', 70000),
 
-    (3, '2021-10-01 15:32:00', 'DV001', 4),
-    (2, '2021-10-01 16:30:00', 'DV001', 4),
-    (1, '2021-10-01 17:15:00', 'DV001', 4)
+    ('DV003', 2, 2, '2021-10-01 15:01:00', 70000),
+    ('DV002', 2, 6, '2021-10-01 15:30:00', 210000),
+    ('DV004', 2, 1, '2021-10-01 16:02:00', 42000),
+
+    ('DV001', 3, 1, '2021-10-01 15:32:00', 30000),
+    ('DV003', 3, 2, '2021-10-01 16:30:00', 70000),
+    ('DV007', 3, 1, '2021-10-01 17:15:00', 32000),
+
+    ('DV002', 4, 2, '2021-10-02 12:10:00', 70000),
+    ('DV001', 4, 4, '2021-10-02 12:10:00', 120000),
+    ('DV005', 4, 1, '2021-10-02 12:10:00', 30000)
 GO
 
 -- chuyển đổi kí tự có dấu thành không dấu và ngược lại
